8f14d30227b96b8419961a6702f58bcf58dc20dc
Mohamed Ali Belkouri <medali.b@devnet.me>
2024-10-29 20:21:00 +0100

    feat:adding  item strcuture

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,16 +1,30 @@
 class Items {
-  final String image;
+  final String img;
   final String title;
   final String subTitle;
 
-  Items({required this.image, required this.title, required this.subTitle});
-
-
-
-  List <Items> listOfItem=[
-    Items(image: "assets/h1.jpg",title: "Hard work always pays off",subTitle: "Im gonna out work everyone nchalah"),
-
-    Items(image: "assets/ippo.jpg", title: "if there is a will , there will always be a way ! ", subTitle: "i will never give up nchalah"),
-    Items(image: "assets/stand.jpg", title: "outworking yourself and always beliving in yourself is the key", subTitle: "i will always persevere , advance,  imporve and work hard , harder than anyone can even imagine")
-  ];
+  ///
+  Items({
+    required this.img,
+    required this.title,
+    required this.subTitle,
+  });
 }
+
+List<Items> listOfItems = [
+  Items(
+    img: "assets/ippo.jpg",
+    title: "Hard work always pays off ! ",
+    subTitle: "Outwork everyone ! ",
+  ),
+  Items(
+    img: "assets/stand.jpg",
+    title: "If there is a will , there wilL always be a way ! ",
+    subTitle: "outwork yourself !!",
+  ),
+  Items(
+    img: "assets/h1.jpg",
+    title: "only healthy food>",
+    subTitle: "only healthy food>",
+  ),
+];
